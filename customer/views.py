f8105df3f1d4559c26fa1823ebea0581cf3b6d6c
Harissh kumar <137392097+Harikowsick@users.noreply.github.com>
2023-10-13 21:02:53 +0530

    Add files via upload

---
+++ customer/views.py
@@ -0,0 +1,132 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.db.models import Q
+from django.core.mail import send_mail
+from .models import MenuItem, Category , OrderModel
+
+class Index(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,'customer/index.html')
+             
+class About(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,'customer/about.html')
+    
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        breakfasts = MenuItem.objects.filter(category__name__contains='Breakfast')
+        lunchs = MenuItem.objects.filter(category__name__contains='Lunch')
+        dinners = MenuItem.objects.filter(category__name__contains='Dinner')
+        drinks = MenuItem.objects.filter(category__name__contains='Drinks')
+        context = {
+            'breakfasts': breakfasts,
+            'lunchs':lunchs,
+            'dinners':dinners,
+            'drinks': drinks,
+        }
+        return render(request, 'customer/order.html', context)
+    
+
+   
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        phone_no = request.POST.get('phone_no')
+        pincode = request.POST.get('pin')
+        
+        order_items ={
+            'items':[]
+        }
+        
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data ={
+                'id': menu_item.pk,
+                'name':menu_item.name,
+                'price':menu_item.price
+            }
+            order_items['items'].append(item_data)
+
+        price = 0
+        item_ids = []
+
+        for item in order_items['items']:
+            item['price']*=2
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+            phone_no=phone_no,
+            pincode=pincode
+            )
+        order.items.add(*item_ids)
+
+        body = ('Thank you for your order!\n' 
+                'Your order is being made and will be delivered to your adress on time\n'
+                f'your order price is : {price}' )
+
+        context = {
+
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return redirect('order-confirmation', pk=order.pk)
+        
+class OrderConfirmation(View):
+    def get(self, request, pk, *arg,**kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk' : order.pk,
+            'items' : order.items,
+            'price' : order.price,
+        }    
+
+        return render(request, 'customer/order_confirmation.html',context)
+    
+    def post(self, request, pk, *args, **kwargs):
+        print(request.body)
+
+class OrderPayConfirmation(View):
+    def post(self, request, pk, *args, **kwargs):
+        return render(request, 'customer/Order_Pay_Confirmation.html')
+
+class Menu(View):
+    def get(self, request, *args, **kwargs):
+        menu_items = MenuItem.objects.all()
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
+
+
+class MenuSearch(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get("q")
+
+        menu_items = MenuItem.objects.filter(
+            Q(name__icontains=query) |
+            Q(price__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
+    
+    
+
